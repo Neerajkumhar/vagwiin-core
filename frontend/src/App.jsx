9a28c65b33fea883ea6011e952d41cb449ea93e3
visuark-core <visuark.info@gmail.com>
2026-02-21 09:46:20 +0530

    Complete Home page responsiveness and connect live products

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ProtectedRoute from './components/ProtectedRoute';
+import ScrollToTop from './components/ScrollToTop';
 import Home from './pages/public/Home';
 import ProductDetail from './pages/public/ProductDetail';
 import Admin from './pages/admin/Admin';
@@ -44,6 +45,7 @@ function App() {
   return (
     <CartProvider>
       <Router>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/shop" element={<Shop />} />
